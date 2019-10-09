dc66f4f1ce22f3f3c15d703b11433b061bef3cbe
Wojciech <profesorprzecinek@wp.pl>
2019-10-09 20:35:36 +0200

    optimization

--- src/main/java/wojtek/HelloWorld.java
+++ src/main/java/wojtek/HelloWorld.java
@@ -6,7 +6,7 @@ import java.time.LocalTime;
 public class HelloWorld {
     public static void main(final String[] args) {
         LocalDate myObj1 = LocalDate.now();
-        System.out.println("/n dzisiaj wieczorem jest:" + myObj1);
+        System.out.println("/n dzisiaj jest:" + myObj1);
         Greetings greetings = new Greetings();
         LocalTime myObj2 = LocalTime.now();
         System.out.println("/n godzina:" + myObj2);
